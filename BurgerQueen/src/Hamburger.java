6df451e252b450dcba054fdf7eabe8c5511c7c56
mingyunLEE <leemingyun@naver.com>
2023-03-02 07:09:28 +0900

    Create Class <Hamurger class>

---
+++ BurgerQueen/src/Hamburger.java
@@ -0,0 +1,25 @@
+public class Hamburger extends Product{
+    private boolean isBurgerSet;
+    private int burgerSetPrice;
+    public Hamburger(int id, String name, int price, int kcal, boolean isBurgerSet, int burgerSetPrice) {
+        super(id, name, price, kcal);
+        this.isBurgerSet = isBurgerSet;
+        this.burgerSetPrice = burgerSetPrice;
+    }
+
+    public void setBurgerSet(boolean burgerSet) {
+        isBurgerSet = burgerSet;
+    }
+
+    public void setBurgerSetPrice(int burgerSetPrice) {
+        this.burgerSetPrice = burgerSetPrice;
+    }
+
+    public boolean isBurgerSet() {
+        return isBurgerSet;
+    }
+
+    public int getBurgerSetPrice() {
+        return burgerSetPrice;
+    }
+}
