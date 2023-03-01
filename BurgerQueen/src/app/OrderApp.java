d09bcf5946bf4bf8cc1ae5c4a8c3a7fd936795a1
mingyunLEE <leemingyun@naver.com>
2023-03-02 08:07:00 +0900

    Modified OrderApp class <add scanner>

--- BurgerQueen/src/app/OrderApp.java
+++ BurgerQueen/src/app/OrderApp.java
@@ -3,16 +3,22 @@ package app;
 import app.product.Product;
 import app.product.ProductRepository;
 
+import java.util.Scanner;
+
 public class OrderApp {
 
     public void start() {
+
+        Scanner scanner = new Scanner(System.in);
+
         ProductRepository productRepository = new ProductRepository();
         Product[] products = productRepository.getAllProducts();
         Menu menu = new Menu(products);
 
         System.out.println("🍔 BergerQueen Order Service");
 //        while (true){
-//          메뉴 출력
+            menu.printMenu();
+            String input = scanner.nextLine();
 //          사용자 입력 받기
 //
 //          if (사용자 입력이 +인 경우){
