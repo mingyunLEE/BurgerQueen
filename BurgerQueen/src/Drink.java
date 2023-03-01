743f082fc6adac69a59697539e1b091871e91b54
mingyunLEE <leemingyun@naver.com>
2023-03-02 07:13:33 +0900

    Create Class <Drink class>

---
+++ BurgerQueen/src/Drink.java
@@ -0,0 +1,16 @@
+public class Drink extends Product{
+    private boolean hasStraw;
+
+    public Drink(int id, String name, int price, int kcal, boolean hasStraw) {
+        super(id, name, price, kcal);
+        this.hasStraw = hasStraw;
+    }
+
+    public boolean HasStraw() {
+        return hasStraw;
+    }
+
+    public void setHasStraw(boolean hasStraw) {
+        this.hasStraw = hasStraw;
+    }
+}
