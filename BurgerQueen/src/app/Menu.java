81fcca18a1200e731bf5efe09b5bce6b19206ed6
mingyunLEE <leemingyun@naver.com>
2023-03-02 08:04:02 +0900

    Modified Menu class <Modified printMenu method>

--- BurgerQueen/src/app/Menu.java
+++ BurgerQueen/src/app/Menu.java
@@ -16,44 +16,52 @@ public class Menu {
         System.out.println("[🔻] 메뉴");
         System.out.println("-".repeat(60));
 
-        System.out.println("🍔 햄버거");
-        for (Product product : products) {
-            if (product instanceof Hamburger){
-                System.out.printf(
-                        "   (%d) %s %5dKcal %5d원\n",
-                        product.getId(), product.getName(), product.getKcal(), product.getPrice()
-                );
-            }
-        }
-        System.out.println();
-
-        System.out.println("🍟 사이드");
-        for(Product product : products){
-            if (product instanceof Side){
-                System.out.printf(
-                        "   (%d) %s %5dKcal %5d원\n",
-                        product.getId(), product.getName(), product.getKcal(), product.getPrice()
-                );
-            }
-        }
-        System.out.println();
-
-        System.out.println("🥤 음료");
-        for(Product product : products){
-            if(product instanceof Drink){
-                System.out.printf(
-                        "   (%d) %s %5dKcal %5d원\n",
-                        product.getId(), product.getName(), product.getKcal(), product.getPrice()
-                );
-            }
-        }
-        System.out.println();
+        printHamburgers();
+        printSides();
+        printDrinks();
 
         System.out.println();
         System.out.println("🧺 (0) 장바구니");
         System.out.println("📦 (+) 주문하기");
         System.out.println("-".repeat(60));
         System.out.print("[📣] 메뉴를 선택해주세요 : ");
+    }
+
+    private void printDrinks() {
+        System.out.println("🥤 음료");
+        for(Product product : products){
+            if(product instanceof Drink){
+                printEachMenu(product);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printSides() {
+        System.out.println("🍟 사이드");
+        for(Product product : products){
+            if (product instanceof Side){
+                printEachMenu(product);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printHamburgers() {
+        System.out.println("🍔 햄버거");
+        for (Product product : products) {
+            if (product instanceof Hamburger){
+                printEachMenu(product);
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printEachMenu(Product product) {
+        System.out.printf(
+                "   (%d) %s %5dKcal %5d원\n",
+                product.getId(), product.getName(), product.getKcal(), product.getPrice()
+        );
     }
 }
 
