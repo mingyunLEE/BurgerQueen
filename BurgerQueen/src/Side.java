0b24201059faa2fed3317cfc200627653b194448
mingyunLEE <leemingyun@naver.com>
2023-03-02 07:12:03 +0900

    Create Class <Side class>

---
+++ BurgerQueen/src/Side.java
@@ -0,0 +1,15 @@
+public class Side extends Product{      // Side class inherited Product class
+    private int ketchup;
+    public Side(int id, String name, int price, int kcal, int ketchup) {
+        super(id, name, price, kcal);
+        this.ketchup = ketchup;
+    }
+
+    public int getKetchup() {
+        return ketchup;
+    }
+
+    public void setKetchup(int ketchup) {
+        this.ketchup = ketchup;
+    }
+}
