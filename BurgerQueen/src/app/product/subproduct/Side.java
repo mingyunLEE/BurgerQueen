ee301e4172faacb8eed78a5eca28b797addae416
mingyunLEE <leemingyun@naver.com>
2023-03-02 15:33:36 +0900

    Create Source code <Side class>

---
+++ BurgerQueen/src/app/product/subproduct/Side.java
@@ -0,0 +1,20 @@
+package app.product.subproduct;
+
+import app.Product;
+
+public class Side extends Product { //side class that inherits from the product class
+    private int ketchup;    //instance variable of Side class
+
+    public Side(int id, String name, int price, int kcal, int ketchup) {       // default class of Side class
+        super(id, name, price, kcal);       // call the upper class constructor
+        this.ketchup = ketchup;     // field variable of Side class
+    }
+
+    public int getKetchup() {       // Getter of ketchup variable
+        return ketchup;
+    }
+
+    public void setKetchup(int ketchup) { //Setter of ketchup variable
+        this.ketchup = ketchup;
+    }
+}
